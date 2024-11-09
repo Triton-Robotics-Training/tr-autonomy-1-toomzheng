bb934eac3f0da0ef31ca6e0a48b1dc41c19ece65
Tom Zheng <t5zheng@ucsd.edu>
2024-11-09 15:26:34 -0800

    finished week 1 training
    submission

--- src/your_solution/src/spin_slow_update.cpp
+++ src/your_solution/src/spin_slow_update.cpp
@@ -1,15 +1,28 @@
 #include "spin_slow_update.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SlowSolution>());
   rclcpp::shutdown();
   return 0;
 }
 
-SlowSolution::SlowSolution() : Node("slowsolution") {
-  RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
-  // your code here
+SlowSolution::SlowSolution() : Node("slowsolution")
+{
+  pos_subscription_ = this->create_subscription<ArrayMsg>(
+      "measuredpos", 10, std::bind(&SlowSolution::pos_callback, this, std::placeholders::_1));
+
+  vel_subscription_ = this->create_subscription<ArrayMsg>(
+      "measuredvel", 10, std::bind(&SlowSolution::vel_callback, this, std::placeholders::_1));
 }
 
-// your code here
+void SlowSolution::pos_callback(const ArrayMsg::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logggiter(), "Received position: [%f, %f]", msg->data[0], msg->data[1]);
+}
+
+void SlowSolution::vel_callback(const ArrayMsg::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "Received velocity: [%f, %f]", msg->data[0], msg->data[1]);
+}
--- src/your_solution/src/spin_slow_update.h
+++ src/your_solution/src/spin_slow_update.h
@@ -9,7 +9,11 @@ class SlowSolution : public rclcpp::Node {
  public:
   SlowSolution();
  private:
-  // your code here
+  void pos_callback(const ArrayMsg::SharedPtr msg);
+  void vel_callback(const ArrayMsg::SharedPtr msg);
+
+  rclcpp::Subscription<ArrayMsg>::SharedPtr pos_subscription_;
+  rclcpp::Subscription<ArrayMsg>::SharedPtr vel_subscription_;
 };
 
 #endif //YOUR_SOLUTION_SRC_SPIN_SLOW_UPDATE_H_
